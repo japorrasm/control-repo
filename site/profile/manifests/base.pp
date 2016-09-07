c7b8ea0268cd3e3d4b65f419261c2ff61a8ce785
japorrasm <juan.a.porras@gmail.com>
2016-09-07 13:13:14 +0200

    Update base.pp

--- site/profile/manifests/base.pp
+++ site/profile/manifests/base.pp
@@ -13,5 +13,22 @@ network::mroute { 'eth0':
     '80.81.82.0/16'  => 'eth0',
   }
 }
-
+class profile::netbackup {
+ archive { '/tmp/r':
+  ensure        => present,
+  extract       => true,
+  extract_path  => '/tmp',
+  source        => 'http://10.147.8.18/Sw/backup/NetBackup_7.7.2_CLIENTS2-tar',
+  creates       => '/tmp/NetBackup_7.7.2_CLIENTS2',
+  cleanup       => true,
+  }
+class { 'netbackup::client':
+    installer       => '/tmp/NetBackup_7.7.2_CLIENTS2/install',
+    version         => '7.7.2',
+    service_enabled => true,
+    masterserver    => 'emhco039.svc',
+    mediaservers    => ['mediasrv1.xyz.com', 'mediasrv2.xyz.com'],
+    excludes        => ['/tmp'],
+}
+}
 }
